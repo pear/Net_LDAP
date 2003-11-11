97d7c486c8920bbdddbcf9c52e8f761f1e315cc7
Tarjei Huse <tarjei@php.net>
2003-11-11 19:39:36 +0000

    Moved the utf8 functions from Net_LDAP_Util to Net_LDAP.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_LDAP/trunk@144146 c90b9560-bf6c-de11-be94-00142212c4b1

--- LDAP.php
+++ LDAP.php
@@ -49,7 +49,7 @@ class Net_LDAP extends PEAR
          'scope' => 'sub'
 
         );
-  
+
     /**
      * The ldap resourcelink.
      * You should not need to touch this.
@@ -57,6 +57,21 @@ class Net_LDAP extends PEAR
      * @var object
      */
      var $_link;
+
+    /**
+     * Net_LDAP_Schema object
+     *
+     * @access private
+     * @var object Net_LDAP_Schema
+     */
+    var $_schema = null;
+    /**
+     * Cache for attribute encoding checks
+     *
+     * @access private
+     * @var array over attributes who should /should not be encoded.
+     */
+    var $_schemaAttrs = array();
 
     /**
      * Net ldap, Sets the configarray and binds if the config array contains the server prameter
@@ -238,7 +253,7 @@ class Net_LDAP extends PEAR
             
             if ($searchresult) {
                 $entries = ldap_get_entries($this -> _link, $searchresult);
-             
+
                 for($i=0;$i<$entries['count'];$i++){
                     $result= $this -> delete ($entries[$i]['dn'],array('recursive' => true));
                     if (!$result) {
@@ -417,7 +432,7 @@ class Net_LDAP extends PEAR
      *                  attributes - (default: none). an array of the attributes that an returned entry should contain.
      *                  
      *                  
-     *                  
+     *
      * [NOT IMPLEMENTED]deref - By default aliases are dereferenced to locate the base object for the search, but not when
      *                          searching subordinates of the base object.
      *                          This may be changed by specifying a "deref" parameter with one of the following values:
@@ -716,6 +731,85 @@ class Net_LDAP extends PEAR
         return $schema;
      }
 }
+
+ /**
+     * Encodes given attributes to UTF8 if needed
+     *
+     * This function takes attributes in an array and then checks against the schema if they need
+     * UTF8 encoding. If that is so, they will be encoded. An encoded array will be returned and
+     * can be used for adding or modifying.
+     *
+     * @access public
+     * @param array $attributes Array of attributes
+     * @return array Array of UTF8 encoded attributes
+     */
+    function utf8Encode( $attributes )
+    {
+        return $this->_utf8($attributes, 'utf8_encode');
+    }
+
+    /**
+     * Decodes the given attribute values
+     *
+     * @access public
+     * @param array attributes Array of attributes
+     * @return array array Array with decoded attribute values
+     */
+    function utf8Decode( $attributes )
+    {
+        return $this->_utf8($attributes, 'utf8_decode');
+    }
+
+    /**
+     * Encodes or decodes attribute values if needed
+     *
+     * @access private
+     * @param array $attributes Array of attributes
+     * @param array $function Function to apply to attribute values
+     * @return array Array of attributes with function applied to values
+     */
+    function _utf8( $attributes, $function )
+    {
+        if (!$this->_schema) {
+            $this->_schema = $this->schema();
+        }
+
+        if (!$this->_ldap || !$this->_schema || !function_exists($function)) {
+           return $attributes;
+        }
+
+        if (is_array($attributes) && count($attributes) > 0)
+        {
+            foreach( $attributes as $k => $v )
+            {
+                if (!isset($this->_schemaAttrs[$k])) {
+
+                  $attr = $this->_schema->get('attribute', $k);
+                  if (Net_LDAP::isError($attr)) {
+                     continue;
+                  }
+
+                  (false !== strpos($attr['syntax'], '1.3.6.1.4.1.1466.115.121.1.15')) ? $encode = true : $encode = false;
+                  $this->_schemaAttrs[$k] = $encode;
+                  }
+
+                } else {
+                  $encode = $this->_schemaAttrs[$k]
+                }
+
+                if ($encode)
+                {
+                    if (is_array($v)) {
+                        foreach( $v as $ak => $av ) $v[$ak] = call_user_func($function, $av );
+                    } else {
+                        $v = call_user_func($function, $v);
+                    }
+                }
+                $attributes[$k] = $v;
+            }
+        }
+        return $attributes;
+    }
 
 
 
