e69f6f60a614d06e36b1410b57835b9fd20e12da
Jan Wagner <jw@php.net>
2004-03-09 12:32:46 +0000

    reflect changes to new tests
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_LDAP/trunk@153236 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/PHPUnit/config.php
+++ tests/PHPUnit/config.php
@@ -1,20 +1,21 @@
 <?php
 
 // these should be valid connection parameters for your ldap server
-$ldap_config= array('host' => '192.168.123.253',
-                    'version' => 3,                    
-                    'base' => 'o=netsols,c=de',
-                    'dn' => 'cn=admin,o=netsols,c=de',
-                    'password' => '******',
+$ldap_config= array('host' => 'server1.netgine.de',
+                    'version' => 3,
+                    'starttls' => false,
+                    'basedn' => 'o=netsols,c=de',
+                    'binddn' => 'cn=admin,o=netsols,c=de',
+                    'bindpw' => '',
                     'filter' => '(objectClass=*)');
 
 // this should be an existing dn which can be fetched with the above connection parameters
-$existing_dn = 'uid=wagner,ou=mailAccounts,cn=netsols.de,ou=Domains,o=netsols,c=de';
-$rename_dn = 'uid=wagner2,ou=mailAccounts,cn=netsols.de,ou=Domains,o=netsols,c=de';
+$existing_dn = 'cn=Jan Wagner,ou=testing,o=netsols,c=de';
+$rename_dn = 'cn=Kai Naumann,ou=testing,o=netsols,c=de';
 
 // these should be parameters for an ldap query that returns at least one entry with one attribute
-$search2 = array('filter' => '(&(objectClass=domainRelatedObject)(domainStatus=active))',
-                 'base'   => 'ou=Domains,o=netsols,c=de',
-                 'parms'  => array('scope' => 'one', 'attributes' => array('cn')));
+$search = array('filter' => '(&(objectClass=ispmanDomain)(ispmanStatus=active))',
+                'base'   => 'ou=ispman,o=netsols,c=de',
+                'parms'  => array('scope' => 'one', 'attributes' => array('cn')));
 
 ?>
