6f5f5b7237bdf8ba28e71dfa8449317dfa2d6ceb
Jan Wagner <jw@php.net>
2005-03-01 12:29:43 +0000

    release 0.6.6
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_LDAP/branches/NET_LDAP_0_6@181091 c90b9560-bf6c-de11-be94-00142212c4b1

--- misc/release.php
+++ misc/release.php
@@ -2,26 +2,30 @@
 
 require_once 'PEAR/PackageFileManager.php';
 
-$version = '0.6.5';
+$version = '0.6.6';
 $notes = 'Bugfix release';
 $changelog = '
-fixed Bug #890 (proper usuage of get_class)
-fixed Bug #952 (Broken Net_LDAP_Entry::add() method)
-changed license to LGPL for now (ongoing discussion on pear-dev)
+Fixed bug #1060 (Wrong array_push_call in Net_LDAP_Search)
+Fixed bug #1136,#2985 (Recursive delete not working)
+Fixed bug #2391 (Implemented getOption()/SetOption() for LDAP_OPT_REFERRALS)
+Fixed bug #2860 (Call-time pass-by-reference)
+Fixed bug #2986 (Missing parameter to ldap_error in LDAP.php)
+Fixed bug #3560 (Missing test.php in docs)
 ';
-
 
 $options = array('changelogoldtonew' => false,
                  'simpleoutput' => true,
                  'packagedirectory' => './',
                  'baseinstalldir' => 'Net',
-                 'ignore' => array('tests/', 'CVS/', 'project.index', 'release.php', '*~'),
+                 'ignore' => array('tests/PHPUnit/', 'CVS/', 'project.index', 'release.php', '*~'),
+		 'exceptions' => array('tests/tests.php' => 'doc'),
                  'dir_roles' => array('doc' => 'doc'),
                  'version' => $version,
                  'state' => 'beta',
                  'notes' => $notes,
                  'changelognotes' => $changelog,
-                 'deps' => array()
+                 'deps' => array(),
+		 'license' => 'LGPL'
                  );
 
 
