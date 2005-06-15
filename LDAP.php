bf30e6c7767009e49ba6a13d248f6449c3c09f2d
Jan Wagner <jw@php.net>
2005-06-15 09:21:23 +0000

    fix bug#4453: static function connect uses $this fix bug#4483: change default behavior of getEntry() to return all attributes
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_LDAP/branches/NET_LDAP_0_6@188442 c90b9560-bf6c-de11-be94-00142212c4b1

--- LDAP.php
+++ LDAP.php
@@ -140,7 +140,7 @@ define ('NET_LDAP_ERROR', 1000);
     function &connect($config = array())
     {
         if (!function_exists('ldap_connect')){
-            return $this->raiseError("It seems that you do not have the ldap-extension installed. Please install it before using this package.");
+            return Net_LDAP::raiseError("It seems that you do not have the ldap-extension installed. Please install it before using this package.");
         }
         @$obj =& new Net_LDAP($config);
         $err  = $obj->bind();
@@ -755,7 +755,7 @@ define ('NET_LDAP_ERROR', 1000);
     * @param array Array of Attributes to select
     * @return object Net_LDAP_Entry or Net_LDAP_Error 
     */
-   function &getEntry($dn, $attr = array(''))
+   function &getEntry($dn, $attr = array())
    {
         $result = $this->search($dn, '(objectClass=*)', array('scope' => 'base', 'attributes' => $attr));
         if (Net_LDAP::isError($result)) {
